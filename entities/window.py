5c4a090f62ddb749a13b80e41e4c8d59ebec3c3b
choctarse <erwan.bassaque@gmail.com>
2020-03-06 13:43:07 +0100

    adding main menu

--- entities/window.py
+++ entities/window.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+
+from pygame import font
 from pygame.locals import *
 
 
@@ -7,17 +9,55 @@ class window:
     def init(self):
         self.start()
 
+
     def start(self):
         pygame.init()
-        maSurface = pygame.display.set_mode((500, 300))
-        pygame.display.set_caption('Space invader')
+        # Chargement du logo et du fond
+        background = pygame.image.load("pictures/bg.png")
+        logo = pygame.image.load("pictures/logo.png")
+        # chargement de la fenetre
+        self.frame = pygame.display.set_mode((500, 700))
+        pygame.display.set_caption('Space invaders')
         inProgress = True
+        # boucle de jeu
         while inProgress:
+            self.frame.blit(background, (0, 0))
+            self.frame.blit(logo, (57.5, 30))
+
+            self.createButtons()
             for event in pygame.event.get():
                 if event.type == QUIT:
                     inProgress = False
-            pygame.display.update()
+            pygame.display.flip()
         pygame.quit()
 
-    def button(self):
-        print('a')
+    def createButtons(self):
+        black = (0, 0, 0)
+        yellow = (255, 231, 9)
+        blue = (71, 63, 132)
+        highlight = (56, 112, 127)
+        pygame.init()
+        mouse = pygame.mouse.get_pos()
+
+        if 73 + 152 > mouse[0] > 73 and 350 + 52 > mouse[1] > 352:
+            pygame.draw.rect(self.frame, highlight, (75, 350, 150, 50))
+        else:
+            pygame.draw.rect(self.frame, yellow, (75, 350, 150, 50))
+
+        if 273 + 152 > mouse[0] > 273 and 350 + 52 > mouse[1] > 352:
+            pygame.draw.rect(self.frame, highlight, (275, 350, 150, 50))
+        else:
+            pygame.draw.rect(self.frame, blue, (275, 350, 150, 50))
+
+        textLaunch = pygame.font.Font("tahoma.ttf", 30).render("Jouer !", True, black)
+        textLaunch.center = ((75 + (100 / 2)), (350 + (50 / 2)))
+        self.frame.blit(textLaunch)
+        pygame.display.flip()
+
+    def getObject(self):
+        return self.frame
+
+    def text_objects(text, font):
+        black = (0, 0, 0)
+        textSurface = font.render("Jouer !", True, black)
+        return textSurface, textSurface.get_rect()
--- main.py
+++ main.py
@@ -3,3 +3,80 @@ from entities.window import window
 
 obj = window()
 obj.init()
+
+# Liste de tout les sprites du jeux ( pour avoir un ensemble, utile pour le coté joueur en code)
+all_sprites_list = pygame.sprite.Group()
+
+# Liste des block (max appel là comme tu le sent juste un réfractoring fonctionnera avec moi)
+block_list = pygame.sprite.Group()
+
+# Liste de chaque balle tiré
+bullet_list = pygame.sprite.Group()
+
+
+# ICI ON INSTANCIE LES BLOCKS (chronologiquement pour que le code ne fasse pas d'erreur)
+
+# On instancie le héro
+hero = Hero()
+all_sprites_list.add(hero)
+
+# La condition d'arrêt du jeu
+done = False
+
+# Pour faire l'actualisation du jeu
+clock = pygame.time.Clock()
+
+score = 0
+hero.rect.y = 370
+
+while not done:
+    # --- Gestion des évènement du jeu
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Tire une balle si on clique
+            bullet = Bullet()
+            # On met la balle là ou est le joueur (le plus 24 c'est pour qu'il soit au centre de l'image)
+            bullet.rect.x = hero.rect.x +24
+            bullet.rect.y = hero.rect.y
+            # On l'ajoute dans le groupe
+            all_sprites_list.add(bullet)
+            bullet_list.add(bullet)
+
+
+    # Permet d'actualiser tout les objet (monstre, héros, balle et + si affinité), ça appel la methode update de chaque obj
+    all_sprites_list.update()
+
+    # Les intéractions et actions de chaque balles
+    for bullet in bullet_list:
+
+        # Vérifie si on se cogne contre un monstre, le non de l'objet + le groupe qu'il cogne (se delete si il se croise)
+        # et on le met dans la liste des block touché
+        block_hit_list = pygame.sprite.spritecollide(bullet, block_list, True)
+
+        # Pour chaque block touché on augmente le score
+        for block in block_hit_list:
+            bullet_list.remove(bullet)
+            all_sprites_list.remove(bullet)
+            score += 1
+            print(score)
+
+        # Enlève la ball quand elle est trop haute sur l'écran à vous de voir quel hauteur précisement
+        if bullet.rect.y < -10:
+            bullet_list.remove(bullet)
+            all_sprites_list.remove(bullet)
+
+
+    # Fait un écran blanc (j'ai fait ça pour mes testes)
+    screen.fill(white)
+
+    # affiche tout les sprites
+    all_sprites_list.draw(screen)
+
+    # Met à jour ce qu'on dessine
+    pygame.display.flip()
+
+
+    clock.tick(20)
